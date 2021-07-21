cfdfb78fcc52647f6a44f4a2b17ac3959bb3f3b7
Никита Поляков <nikitapolyakov@MacBook-Air-Nikita-Polakov.local>
2021-07-21 12:03:39 +0300

    add form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostFormComponent } from './components/post-form/post-form.component';
+import { PostComponent } from './components/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostFormComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
