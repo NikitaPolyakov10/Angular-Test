7d7a6f2402e27df97d8b624eaebfc8f8d8f90b35
Никита Поляков <nikitapolyakov@MacBook-Air-Nikita-Polakov.local>
2021-07-22 16:43:42 +0300

    pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MultByPipe } from './pipes/mult-by.pipe';
+import { ExMarksPipe } from './pipes/ex-marks.pipe';
+import { FilterPipe } from './pipes/filter.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
+  declarations: [AppComponent, MultByPipe, ExMarksPipe, FilterPipe],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
